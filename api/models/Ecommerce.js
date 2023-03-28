629415670a86be57ce82d6501a3da85d7a9a995a
nahuel di santi <nahuel.disanti@gmail.com>
2023-03-28 01:07:36 -0300

    configuracion de persistencia en Mongo

--- api/models/Ecommerce.js
+++ api/models/Ecommerce.js
@@ -9,20 +9,12 @@ module.exports = {
 
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+      //id: {type:'string',columnName:'_id', autoIncrement: false },
+      name: {type: 'string', required: true},
+      price: {type: 'number', required: true},
+      desc: {type: 'string', required: true},
+      img: {type: 'string', required: true}, 
+      stock: {type: 'number', required: true}
   },
 
 };
